5616259a4933580a0e67a02a075e4f60dd0b4b34
Takahiko Wada <wada.takahiko@gmail.com>
2024-08-06 16:08:56 +0400

    initial commit

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    // ...add more providers here
+  ],
+  callbacks: {
+    async signIn(params) {
+      console.log(params);
+      return true;
+    },
+  },
+};
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
